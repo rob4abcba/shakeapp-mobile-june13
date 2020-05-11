a7738eda1b714fbb88a0bcd5b257b35d33951763
R <rob_abcba@yahoo.com>
2020-05-11 07:15:09 -0700

    Paste CnctyCube config.j to bott of sh-mo config.j

--- src/main/web/config.js
+++ src/main/web/config.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   StatusBar,
 } from 'react-native';
+import { ConnectyCube } from 'react-native-connectycube';
 
 const {width, height} = Dimensions.get('window');
 const realWidth = height > width ? width : height;
@@ -35,3 +36,51 @@ module.exports = {
   homeNeedsUpdate: false,
   analyticsNeedsUpdate: false,
 };
+
+
+
+
+
+// From ConnectyCube
+export default [
+  {
+    appId: 2429,
+    authKey: "UMKeOS3aUGBNgcF",
+    authSecret: "Spg7Kd2ZQMmUCQE"
+  },
+  {
+    debug: {mode: 1},
+  },
+];
+
+export const users = [
+  {
+    id: 72780,
+    name: 'Alice',
+    login: 'videouser1',
+    password: 'videouser1',
+    color: '#34ad86',
+  },
+  {
+    id: 72781,
+    name: 'Bob',
+    login: 'videouser2',
+    password: 'videouser2',
+    color: '#077988',
+  },
+  {
+    id: 590565,
+    name: 'Ciri',
+    login: 'videouser3',
+    password: 'videouser3',
+    color: '#13aaae',
+  },
+  {
+    id: 590583,
+    name: 'Dexter',
+    login: 'videouser4',
+    password: 'videouser4',
+    color: '#056a96',
+  },
+];
+
